3e4bd23feb7354cb5c450b1dd9e8a4cd0a0b87e4
Matheus-TecDev <matheus.tecnodev@gmail.com>
2025-11-10 19:49:18 -0300

    Upgrade das partes vitais e essenciais do projeto, com inserção de fotos e propriedades de texto e estilizações necessárias  efinalização do projeto

--- app/src/main/java/com/example/mercado/MainActivity.java
+++ app/src/main/java/com/example/mercado/MainActivity.java
@@ -1,24 +1,42 @@
 package com.example.mercado;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox cbCafe, cbPicanha, cbOvo, cbRefri;
+    Button btnCalcular;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        cbCafe = findViewById(R.id.cbCafe);
+        cbPicanha = findViewById(R.id.cbPicanha);
+        cbOvo = findViewById(R.id.cbOvo);
+        cbRefri = findViewById(R.id.cbRefri);
+        btnCalcular = findViewById(R.id.btnCalcular);
+
+        btnCalcular.setOnClickListener(v -> {
+            double total = 0;
+
+            if (cbCafe.isChecked()) total += 16.50;
+            if (cbPicanha.isChecked()) total += 69.90;
+            if (cbOvo.isChecked()) total += 12.00;
+            if (cbRefri.isChecked()) total += 12.00;
+
+            String msg = "Total de compras: R$ " + String.format("%.2f", total);
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Mix Matheus")
+                    .setMessage(msg)
+                    .setPositiveButton("OK", null)
+                    .show();
         });
     }
 }
